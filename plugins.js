1693a4a069be0b141cbb517c402fd2d05a8c23aa
januariov182-droid <januariov182@gmail.com>
2026-06-17 21:52:46 -0300

    Update plugins.js

--- plugins.js
+++ plugins.js
@@ -103,7 +103,7 @@ const plugins = {
   level_system: {
     name: "Level System XP",
     emoji: "⭐",
-    price: 00.1,
+    price: 0.01,
     shortDesc: "Sistema de XP e ranking para engajar membros",
     description:
       "Engaje sua comunidade com o **Level System XP**. Membros ganham XP ao enviar " +
